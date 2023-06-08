957a9addd38264e3d9938f9897d85ae7bd763cd9
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-06-08 08:46:42 +0200

    Created DWA6

--- DWA_5/script.js
+++ DWA_5/script.js
@@ -7,19 +7,25 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  if (dividend !== Number || divider !== Number){
-    document.body.innerText = 'Something critical went wrong. Please reload the page.'
-    throw new Error ('NaN')
-  }
-
+  const answer = dividend / divider
+ try {
   if (dividend === '' || divider === '') {
     result.innerText = 'Division not performed. Both values are required in inputs. Try again.'
     return 
   }
+
+  if (isNaN(answer)){
+    document.body.innerText = 'Something critical went wrong. Please reload the page.'
+    throw new Error ('Cannot calculate non-numeric values')
+  }
+
   if (divider < 0 ) {
     result.innerText = 'Division not performed. Invalid number provided. Try again.'
     throw new Error ('Invalid division')
   }
-  result.innerText = parseFloat(Math.floor(dividend / divider)).toFixed(0);
+} catch (error) {
+    console.error (error.message)
+}
+  result.innerText = parseFloat(Math.floor(answer)).toFixed(0);
   
 });
