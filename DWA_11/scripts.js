f302f35ee38ed49c88b8cab3a2ca93f377d979b5
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-06-26 14:37:40 +0200

    Added DWA_12

--- DWA_11/scripts.js
+++ DWA_11/scripts.js
@@ -3,12 +3,11 @@ import { incrementCount, decrementCount, resetNumber } from "./model/actions.js"
 
 subscribe((_, next)=> console.log (next))
 console.log (getState())
-dispatch(incrementCount())
-dispatch(incrementCount())
-dispatch(incrementCount())
-dispatch(resetNumber())
-console.log (getState())
 // dispatch(incrementCount())
+// dispatch(incrementCount())
+// dispatch(decrementCount())
 // dispatch(resetNumber())
+// console.log (getState())
+
 
 
