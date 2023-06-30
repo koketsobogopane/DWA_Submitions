29f1b304eb0fa669740dcc660fc2cfd68c483798
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-06-30 14:34:16 +0200

    Update DWA_14

--- DWA_14/components/tally-app.js
+++ DWA_14/components/tally-app.js
@@ -1,12 +1,59 @@
 import {LitElement, html} from "../libs/lit-html.js"
 
 class App extends LitElement {
+    
+    static properties = {
+        open: {type: 'boolean', state: true},
+        value: {type: 'number', state: 0 }
+    }
+
+    constructor() {
+        super();
+        this.open = false
+        this.value = 0
+        
+    }
+
+    increment () {
+        this.value++
+
+    }
+    decrement () {
+        this.value--
+     
+    }
+
+    reset () {
+        this.value = 0 
+        this.open = !this.open
+
+    }
+
     /**
      * 
      * @returns {any}
      */
-    return() {
-        return html` <div>123</div>`
+    render() {
+        return html`  
+        <sl-dialog  open no-header>
+        <div class="counterDisplay">
+            <sl-input size="small" filled readonly value =${this.value} data-key="number"></sl-input>
+        </div>
+        <div class="counterButtons">
+        <sl-button @click = ${this.increment}>+</sl-button>
+        <sl-button @click = ${this.decrement}>-</sl-button>
+        </div>
+        <div class="resetButton">
+        <sl-button size="medium" pill @click = ${this.reset} >Reset</sl-button>
+        <sl-dialog open = ${this.open} >
+        <sl-alert variant="warning" open  >
+            <sl-icon slot="icon" name="exclamation-triangle" ></sl-icon>
+            The counter has been reset 
+          </sl-alert>
+          </sl-dialog>
+          
+        </div>
+    </sl-dialog>`
     }
 }
 
