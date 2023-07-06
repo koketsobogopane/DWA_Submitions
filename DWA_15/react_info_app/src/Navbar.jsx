20b051f64125bd437265c46eb1183fd970067371
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-07-06 14:31:17 +0200

    Updated App.css and Navbar.jsx

--- DWA_15/react_info_app/src/Navbar.jsx
+++ DWA_15/react_info_app/src/Navbar.jsx
@@ -1,9 +1,9 @@
-
+import ReactLogo from "./assets/react-icon-small.png"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="" className="nav--icon" />
+            <img srcSet={ReactLogo} className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
