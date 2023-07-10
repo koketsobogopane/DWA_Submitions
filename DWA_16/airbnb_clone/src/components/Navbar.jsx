063722a78b77d9d05cce5ff08b30207f60f22224
koketsobogopane <koketsobogopanenov24@gmail.com>
2023-07-10 13:30:32 +0200

    updated DWA_16

---
+++ DWA_16/airbnb_clone/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import airbnbLogo from "../assets/images/airbnb-logo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={airbnbLogo} className="nav--logo"/>
+        </nav>
+    )
+}
